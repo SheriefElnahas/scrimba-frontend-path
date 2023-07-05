a7ba7c04c2078602af789c0f6432052c07fba066
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-07-05 22:43:43 +0300

    dynamic alert message & read only password inputs

--- 03-password-generator/app.js
+++ 03-password-generator/app.js
@@ -1,4 +1,4 @@
-// ************************************ Selections ************************************
+// ************************************ Selections Start  ************************************
 // Select Password Range Element & Generate Buttons & The Password Input
 const numberInput = document.querySelector('input[type="number"]');
 const btn = document.querySelector('button');
@@ -9,58 +9,70 @@ const secondPasswrodInput = document.querySelector('.input-container input:nth-c
 let lettersEl = document.querySelector('input[value="letters-element"]');
 let symbolsEl = document.querySelector('input[value="symbols-element"]');
 let numbersEl = document.querySelector('input[value="numbers-element"]');
-// ************************************ Selections ************************************
-
-
-
+// ************************************ Selections End ************************************
 
 // Arrays Needed
-const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', ',', '|', ':', ';', '<', '>', '.', '?', '/'];
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let finalArr = [];
 
+let password = '';
 
-let password = "";
+// ************************************ Helper Functions Start ************************************
 
+// Dynamic Alert Text
+function showAlertText(alertMessage) {
+  const alertText = document.querySelector('.alert__text');
 
-// ************************************ Helper Functions ************************************
+  alertText.textContent = alertMessage;
+
+  if (!lettersEl.checked && !symbolsEl.checked && !numbersEl.checked) {
+    alertText.style.visibility = 'visible';
+  } else {
+    alertText.style.visibility = 'hidden';
+  }
+}
+
 // Build The Final Array That we will use to generate the password
 function filterConditions() {
-    if(lettersEl.checked) {
-        finalArr = finalArr.concat(letters);
-    } 
-     if(symbolsEl.checked) {
-        finalArr =  finalArr.concat(symbols);
-    }   if(numbersEl.checked) {
-        finalArr =   finalArr.concat(numbers);
-    }
-   
+  // Show alert text if the user didn't select any field
+  showAlertText('Please Select Any Field!');
+
+  if (lettersEl.checked) {
+    finalArr = finalArr.concat(letters);
+  }
+  if (symbolsEl.checked) {
+    finalArr = finalArr.concat(symbols);
+  }
+  if (numbersEl.checked) {
+    finalArr = finalArr.concat(numbers);
+  }
 }
 
 // Build the password based on the final array
 function generateRandomPassword(arr, length) {
-    let password = "";
-    for(let i = 0; i < length; i++) {
-        
-        let random = Math.floor(Math.random() * arr.length);
-        password += arr[random];
-    }
-    return password;
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    let random = Math.floor(Math.random() * arr.length);
+    password += arr[random];
+  }
+  return password;
 }
-// ************************************ Helper Functions ************************************
+// ************************************ Helper Functions End ************************************
 
+// ************************************ Show Alert Text Start  ************************************
 
+// ************************************ Show Alert Text End  ************************************
 
 btn.addEventListener('click', () => {
-    // Figure out the conditions by calling filter condition function 
-    filterConditions();
+  // Figure out the conditions by calling filter condition function
+  filterConditions();
 
-    // Check to see if the users has chosen any checkbox or not
-    if(finalArr.length != 0) {
-        //Fill the password field with the random generator password.
-        firstPasswordInput.value =  generateRandomPassword(finalArr, numberInput.value);
-        secondPasswrodInput.value =  generateRandomPassword(finalArr, numberInput.value);    
-    }   
-})
+  // Check to see if the users has chosen any checkbox or not
+  if (finalArr.length != 0) {
+    //Fill the password field with the random generator password.
+    firstPasswordInput.value = generateRandomPassword(finalArr, numberInput.value);
+    secondPasswrodInput.value = generateRandomPassword(finalArr, numberInput.value);
+  }
+});
