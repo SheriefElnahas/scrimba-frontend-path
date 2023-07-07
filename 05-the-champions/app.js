d0221cf954d4505839a5649bcd14fe42d475f10d
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-07-07 14:13:26 +0300

    code refactor

--- 05-the-champions/app.js
+++ 05-the-champions/app.js
@@ -1,14 +1,6 @@
-const endorsementMessage = document.querySelector('.card__textarea');
-const endorsementFrom = document.querySelector('#from');
-const endorsementTo = document.querySelector('#to');
-const form = document.querySelector('form');
-const cardBtn = document.querySelector('.card__btn');
-const endorsementsElement = document.querySelector('.endorsements');
-
-const endorsementArray = [];
-
+// Firebase Imports
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 const appSettings = {
   databaseURL: 'https://endorsements-132d8-default-rtdb.europe-west1.firebasedatabase.app/',
@@ -18,28 +10,43 @@ const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const endorsementsInDB = ref(database, 'endorsements');
 
-form.addEventListener('submit', (e) => {
+// Endorsement Form & Content
+const endorsementForm = document.querySelector('form');
+const endorsementMessage = document.querySelector('.card__textarea');
+const endorsementFrom = document.querySelector('#from');
+const endorsementTo = document.querySelector('#to');
+const endorsementsContainerElement = document.querySelector('.endorsements');
+const addEndoresmentBtn = document.querySelector('#add-endorestment-btn');
+
+endorsementForm.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-cardBtn.addEventListener('click', () => {
+// const endorsementArray = [];
+addEndoresmentBtn.addEventListener('click', () => {
+  // Extract Endorsement Values
   const endorsementMessageValue = endorsementMessage.value;
   const fromValue = endorsementFrom.value;
   const toValue = endorsementTo.value;
 
+  // Create A New Endorsement Object
   const newendorsement = {
     endorsementMessage: endorsementMessageValue,
     from: fromValue,
     to: toValue,
   };
 
+  // Push this Endorsement object to firebase
   push(endorsementsInDB, newendorsement);
+  // Push a new endorsement to firebase
 
-  endorsementArray.push(newendorsement);
+  // endorsementArray.push(newendorsement);
 
+  // Clear the inputs
   endorsementMessage.value = endorsementFrom.value = endorsementTo.value = '';
 
-  endorsementsElement.innerHTML += createNewEndorsement(endorsementMessageValue, fromValue, toValue);
+  // Create A New Endorsement and add it to the HTML
+  endorsementsContainerElement.innerHTML += createNewEndorsement(endorsementMessageValue, fromValue, toValue);
 });
 
 function createNewEndorsement(endorsementMessage, from, to) {
